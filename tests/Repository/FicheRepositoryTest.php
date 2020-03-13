96b7f5c0e0b4d3fb22e69593edd50faec3919b1e
Sacha Flachet <sflachet@sio-savary-85.local>
2020-03-13 14:41:01 +0100

    tests

--- tests/Repository/FicheRepositoryTest.php
+++ tests/Repository/FicheRepositoryTest.php
@@ -1,22 +1,23 @@
 <?php
 
-namespace App\Tests\Repository; 
+namespace App\Tests\Repository;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\Repository\FicheRepository;
 
-class SerieControllerTest extends WebTestCase{
-    
-    public function testFindByMonth(){
+class SerieControllerTest extends WebTestCase
+{
+
+    public function testFindByMonth()
+    {
         self::bootKernel();
         $nb = count(self::$container->get(FicheRepository::class)->findByMonth(3));
         $this->assertEquals(10, $nb);
     }
-
-    public function testFindByDate(){
+    public function testFindByDate()
+    {
         self::bootKernel();
         $nb = count(self::$container->get(FicheRepository::class)->findByDate("2020-03-13"));
         $this->assertEquals(10, $nb);
     }
-
 }
